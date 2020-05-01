4871026725c3f3c69b2d6bc2f1cdf29c1bd10295
Klendi Gocci <klendigocci@gmail.com>
2020-05-01 17:52:08 +0200

    Add TS rewrite

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
-import { ExampleComponent } from 'react-top-loading-bar'
+import { LoadingBar } from 'react-top-loading-bar'
 import 'react-top-loading-bar/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [progress2, setProgress] = useState(0)
+  const refContainer = useRef(null)
+
+  return (
+    <div>
+      <LoadingBar
+        progress={progress2}
+        shadow={true}
+        ref={refContainer}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      {console.log('REf container is', refContainer)}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <button onClick={() => setProgress(progress2 + 50)}>Increase</button>
+      <button onClick={() => setProgress(progress2 - 10)}>Decrease</button>
+      <h1>Hello world</h1>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,110 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import { CSSProperties, useEffect, useState } from 'react'
 
-interface Props {
-  text: string
+type IProps = {
+  progress: number
+  color?: string
+  shadow?: boolean
+  background?: string
+  height?: number
+  onLoaderFinished?: Function
+  className?: string
+  loaderSpeed?: number
+  transitionTime?: number
+  waitingTime?: number
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const LoadingBar = ({
+  progress = 0,
+  height = 2,
+  className = '',
+  color = 'red',
+  background = 'transparent',
+  onLoaderFinished,
+  transitionTime = 300,
+  loaderSpeed = 500,
+  waitingTime = 1000,
+  shadow = true,
+}: IProps) => {
+  const initialLoaderStyle: CSSProperties = {
+    height: '100%',
+    background: color,
+    transition: `all ${loaderSpeed}ms ease`,
+  }
+
+  const loaderContainerStyle: CSSProperties = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    height,
+    background,
+    zIndex: 99999999999,
+    width: 100 + '%',
+  }
+
+  const initialShadowStyles: CSSProperties = {
+    boxShadow: `0 0 10px ${color}, 0 0 5px ${color}`,
+    width: '5%',
+    opacity: 1,
+    position: 'absolute',
+    height: '100%',
+    transition: `all ${loaderSpeed}ms ease`,
+    transform: 'rotate(3deg) translate(0px, -4px)',
+  }
+  const [loaderStyle, loaderStyleSet] = useState(initialLoaderStyle)
+  const [shadowStyle, shadowStyleSet] = useState(initialShadowStyles)
+
+  const checkIfFull = () => {
+    if (progress >= 100) {
+      //now it should wait a little bit
+      loaderStyleSet({
+        ...loaderStyle,
+        width: '100%',
+      })
+      shadowStyleSet({
+        ...shadowStyle,
+        left: progress - 5.5 + '%',
+      })
+
+      setTimeout(() => {
+        //now it can fade out
+        loaderStyleSet({
+          ...loaderStyle,
+          opacity: 0,
+          width: '100%',
+          transition: `opacity ${transitionTime}ms ease-out`,
+        })
+
+        setTimeout(() => {
+          //here we wait for it to fade
+          if (onLoaderFinished) onLoaderFinished()
+        }, transitionTime)
+      }, waitingTime)
+    } else {
+      loaderStyleSet({
+        ...loaderStyle,
+        width: progress + '%',
+        opacity: 1,
+        transition: progress > 0 ? `all ${loaderSpeed}ms ease` : '',
+      })
+
+      shadowStyleSet({
+        ...shadowStyle,
+        left: progress - 5.5 + '%',
+        transition: progress > 0 ? `all ${loaderSpeed}ms ease` : '',
+      })
+    }
+  }
+
+  useEffect(() => {
+    checkIfFull()
+  }, [progress])
+
+  return (
+    <div className={className} style={loaderContainerStyle}>
+      <div style={loaderStyle}>
+        {shadow ? <div style={shadowStyle} /> : null}
+      </div>
+    </div>
+  )
 }
