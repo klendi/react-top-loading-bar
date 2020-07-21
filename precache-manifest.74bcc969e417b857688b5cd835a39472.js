78696325f75106737b49bf3a928d508b5d68c177
Klendi Gocci <klendigocci@gmail.com>
2020-07-21 02:23:32 +0200

    Updates

---
+++ precache-manifest.74bcc969e417b857688b5cd835a39472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95cff1f7c8c63772053cdcf86ea1733",
+    "url": "/react-top-loading-bar/index.html"
+  },
+  {
+    "revision": "71ea77f860d7295fff34",
+    "url": "/react-top-loading-bar/static/css/main.3ad255aa.chunk.css"
+  },
+  {
+    "revision": "b6c81bf6371d28d773d2",
+    "url": "/react-top-loading-bar/static/js/2.aa867f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-top-loading-bar/static/js/2.aa867f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ea77f860d7295fff34",
+    "url": "/react-top-loading-bar/static/js/main.3ca39565.chunk.js"
+  },
+  {
+    "revision": "8116a3bdb49aff4d8939",
+    "url": "/react-top-loading-bar/static/js/runtime-main.bf5a9bcb.js"
+  }
+]);
